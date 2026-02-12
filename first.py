358df4a30b04ce9b9c0ed822cfb36627646476ef
2137rraut <158799229+2137rraut@users.noreply.github.com>
2026-02-12 22:41:48 +0530

    add first.py

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
 print("Hi there, this is first program");
+#int,float
+#str
+#bool
+#None
+
+status_code=200
+response_time=1.25
+message='success'
+is_valid=True
+result=None
+
+print(type(status_code))
+print(type(response_time))
+print(type(message))
+print(type(is_valid))
+print(type(result))
